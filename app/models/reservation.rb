9eebf3ce05327ead437fb25943003325090375d2
Emeline Barthe <bartheemeline@gmail.com>
2022-12-15 15:23:33 +0100

    correction

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,7 +6,8 @@ class Reservation < ApplicationRecord
   private
 
   def checkout_checkin
-    return if listing.missions.find_by(mission_type: :last_checkout, date: end_date)
+    return if listing.missions.find_by(mission_type: "last_checkout", date: end_date)
+
     Mission.create(listing_id: self.listing_id, mission_type: "checkout_checkin", date: (self.end_date).strftime("%Y-%m-%d"), price: self.listing.num_rooms * 10)
   end
 end
