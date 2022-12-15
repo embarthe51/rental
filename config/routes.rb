aa40fff1ba67ea4f0f1d78f70f9b76f34df96c9c
Emeline Barthe <bartheemeline@gmail.com>
2022-12-15 15:09:38 +0100

    condition if return ajoutee

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :listings
+      resources :listings, only: [:index, :show, :create, :update, :delete]
       resources :missions, only: [:index]
     end
   end
